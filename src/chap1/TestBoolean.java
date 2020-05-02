374c28f25744189e5541586053b2052359bf2265
khanhTran <khanhtrantung190894@gmail.com>
2020-05-02 09:56:09 +0700

    add file

---
+++ src/chap1/TestBoolean.java
@@ -0,0 +1,9 @@
+package chap1;
+
+public class TestBoolean {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.print(i);
+        }
+    }
+}
